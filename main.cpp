9d7216830f6cad516ec28b3142ec5c81c8c5bf0f
yur000 <admin@cosmostv.tk>
2014-05-23 16:52:17 +0300

    Added checkbox in a "Blocked" row

--- main.cpp
+++ main.cpp
@@ -7,25 +7,33 @@
 #include <QTableWidget>
 #include <QStringList>
 #include <QMenu>
+#include <QCheckBox>
 #include "header.h"
 
 int main (int argc, char** argv)
 {
-    int n = 3;
+    int n = 5;
     QApplication        app(argc, argv);
     QWidget             mainWindow;
     QLayout             *layout = new QBoxLayout(QBoxLayout::TopToBottom);
     QTableWidget        table(n, n);
+    QCheckBox           *chkbox;
     QTableWidgetItem    *ptwi;
     QStringList         strList;
 
-    strList << "Number" << "Balance" << "Internet";
+    strList << "Number" << "Balance" << "Tarif" << "Internet" << "Blocked";
     //TEST
     table.setHorizontalHeaderLabels(strList);
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            ptwi = new QTableWidgetItem(QString("%1,%2").arg(i).arg(j));
-            table.setItem(i, j, ptwi);
+            if(j!=4) {
+                ptwi = new QTableWidgetItem(QString("%1,%2").arg(i).arg(j));
+                table.setItem(i, j, ptwi);
+            }
+            else {
+                chkbox = new QCheckBox;
+                table.setCellWidget(i, j, chkbox);
+            }
         }
     }
 
