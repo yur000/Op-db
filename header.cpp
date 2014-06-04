aa47975986ca5da2ff59d0d13e7dc4487fb2485a
yur000 <admin@cosmostv.tk>
2014-06-04 03:55:10 +0300

    Added container, slot "changed", rebuilded table

--- header.cpp
+++ header.cpp
@@ -1,12 +1,10 @@
 #include "header.h"
 
-// ----------------------------------------------------------------------
 Counter::Counter() : QObject()
                    , m_nValue(0)
 {
 }
 
-// ----------------------------------------------------------------------
 void Counter::slotInc()
 {
     emit counterChanged(++m_nValue);
@@ -15,4 +13,80 @@ void Counter::slotInc()
         emit goodbye();
     }
 }
+// ----------------------------------------------------------------------
+mainWindow::mainWindow(QWidget* pwgt) {
 
+    lbl         = new QLabel("qqqq");
+    add         = new QPushButton("Add");
+    close       = new QPushButton("Close");
+    tbl         = new QTableWidget(pwgt);
+    layout      = new QBoxLayout(QBoxLayout::TopToBottom);
+    layoutbut   = new QBoxLayout(QBoxLayout::LeftToRight);
+    iter        = new QMap<QString, element>::iterator;
+    *iter       = numbers.begin();
+    strList     << "Number"
+                << "Balance"
+                << "Tarif"
+                << "Internet"
+                << "Blocked";
+    //TEST
+    int n = 5;
+
+
+    element *el = new element(111,22,0,1);
+
+    numbers.insert("2000001", *el);
+
+    tbl->setRowCount(numbers.size());
+    tbl->setColumnCount(n);
+    tbl->setHorizontalHeaderLabels(strList);
+    int i=0;
+    *iter = numbers.begin();
+    for (i=0; i<numbers.size(); i++) {
+        ptwi = new QTableWidgetItem(QString(iter->key()));
+        tbl->setItem(i, 0, ptwi);
+
+        ptwi = new QTableWidgetItem(QString::number(iter->value().getBalance()));
+        tbl->setItem(i, 1, ptwi);
+
+        ptwi = new QTableWidgetItem(QString::number(iter->value().getTarifID()));
+        tbl->setItem(i, 2, ptwi);
+
+        chkbox = new QCheckBox;
+        chkbox->setChecked(iter->value().isInet());
+        tbl->setCellWidget(i, 3, chkbox);
+
+        chkbox = new QCheckBox;
+        chkbox->setChecked(iter->value().isBlock());
+        tbl->setCellWidget(i, 4, chkbox);
+    }
+
+    layoutbut->addWidget(add);
+    layoutbut->addWidget(close);
+    layout->addItem(layoutbut);
+    layout->addWidget(tbl);
+    layout->addWidget(lbl);
+
+    this->setLayout(layout);
+    this->setMinimumHeight(300);
+    this->setMinimumWidth(550);
+    this->show();
+
+    connect(tbl, SIGNAL(cellChanged(int,int)),
+                       SLOT(changed(int,int)));
+    connect(&cnt, SIGNAL(counterChanged(int)),
+                             lbl, SLOT(setNum(int)));
+    connect(close, SIGNAL(clicked()), SLOT(close()));
+
+}
+mainWindow::~mainWindow() {
+}
+
+void mainWindow::changed(int i, int j){
+    if(j==2) {
+        ptwi = tbl->item(i,0);
+        *iter = numbers.find(ptwi->text());
+        ptwi = tbl->item(i,j);
+        iter->value().setTarifID(ptwi->text().toInt());
+    }
+}
--- header.h
+++ header.h
@@ -2,6 +2,17 @@
 #define HEADER_H
 
 #include <QObject>
+#include <QString>
+#include <QWidget>
+#include <QLabel>
+#include <QPushButton>
+#include <QLayout>
+#include <QTableWidget>
+#include <QStringList>
+#include <QMenu>
+#include <QCheckBox>
+#include <QMap>
+#include "element.h"
 
 class Counter : public QObject {
     Q_OBJECT
@@ -17,6 +28,28 @@ public slots:
 signals:
     void goodbye       (   );
     void counterChanged(int);
+};
+
+class mainWindow : public QWidget {
+    Q_OBJECT
+private:
+    QLabel                            *lbl;
+    QTableWidget                      *tbl;
+    QCheckBox                         *chkbox;
+    QTableWidgetItem                  *ptwi;
+    QStringList                       strList;
+    QPushButton                       *add;
+    QPushButton                       *close;
+    Counter                           cnt;
+    QLayout                           *layout;
+    QLayout                           *layoutbut;
+    QMap<QString, element>            numbers;
+    QMap<QString, element>::iterator  *iter;
+public:
+    mainWindow(QWidget* pwgt = 0);
+    virtual ~mainWindow();
+private slots:
+    void changed(int,int);
 };
 
 #endif // HEADER_H
